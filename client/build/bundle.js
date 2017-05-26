1058bf2e0c26231a2f19776c43c63a240c953a10
Benjamin <benjamin.kinnard1@gmail.com>
2017-05-26 14:12:19 +0100

    populate weather now works

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -121,6 +121,7 @@ var UI = function(){
   mapDiv.style.width = "500px";
   var center = {lat: -42.570323, lng: 172.146130}
   this.mainMap = new MapWrapper(center, 5, mapDiv)
+  this.requestHelper = new RequestHelper();
 }
 
 UI.prototype = {
@@ -138,18 +139,33 @@ UI.prototype = {
   render: function(fixtures){
     var container = document.getElementById("fixtures-container");
     container.innerHTML = "";
-    console.log(fixtures);
     for (var fixture of fixtures) {
+      url = "http://api.openweathermap.org/data/2.5/weather?lat=" + fixture.stadium.latLng.lat + "&lon=" + fixture.stadium.latLng.lng + "&appid=d1da5efdf6bd32c103ff303597e79de2";
+      this.requestHelper.makeRequest(url, this.populateWeather )
       var li = document.createElement('li');
       this.appendText(li, fixture.homeTeamName, 'Home: ');
       this.appendText(li, fixture.awayTeamName, 'Away: ');
 
       container.appendChild(li);
     }
+  },
+  populateWeather: function(location){
+    console.log(location)
+    var ul = document.getElementById('weatherAPI');
+    var li = document.createElement('li')
+    li.innerText = "Weather type: " + location.weather[0].main
+
+    var li2 = document.createElement('li')
+    var tempCelsius = Math.round((location.main.temp - 273.15))
+    li2.innerText = "Temperature (celsius): " + tempCelsius
+
+    ul.appendChild(li)
+    ul.appendChild(li2)
   }
 }
 
 module.exports = UI;
+
 
 /***/ }),
 /* 2 */
@@ -259,49 +275,7 @@ module.exports = MapWrapper;
 /* 6 */
 /***/ (function(module, exports) {
 
-var weather = function() {
-
-  var url3 = urlConstructor(-35.734070, 174.329517);
-
-  makeRequest(url3, requestComplete)
-
-}
-
-
-var urlConstructor = function(lat, lng){
-  url = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lng + "&appid=d1da5efdf6bd32c103ff303597e79de2";
-  return url;
-}
-
-var requestComplete = function(){
-  if(this.status !== 200) return;
-
-  var jsonString = this.responseText;
-  var location = JSON.parse(jsonString);
-  populateWeather(location);
-}
-
-var makeRequest = function(url, callback){
-  var request = new XMLHttpRequest()
-  request.open("GET", url);
-  request.addEventListener("load", callback);
-  request.send();
-}
-
-var populateWeather = function(location){
-  var ul = document.getElementById('weatherAPI');
-  var li = document.createElement('li')
-  li.innerText = "Weather type: " + location.weather[0].main
-
-  var li2 = document.createElement('li')
-  var tempCelsius = Math.round((location.main.temp - 273.15))
-  li2.innerText = "Temperature (celsius): " + tempCelsius
-
-  ul.appendChild(li)
-  ul.appendChild(li2)
-}
-
-window.addEventListener('load', weather);
+throw new Error("Module build failed: Error: ENOENT: no such file or directory, open '/Users/user/Desktop/cx3-11/homeworks/project3/The-Lions-App/client/src/views/weather.js'");
 
 /***/ })
 /******/ ]);
--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -13,6 +13,7 @@ var UI = function(){
   mapDiv.style.width = "500px";
   var center = {lat: -42.570323, lng: 172.146130}
   this.mainMap = new MapWrapper(center, 5, mapDiv)
+  this.requestHelper = new RequestHelper();
 }
 
 UI.prototype = {
@@ -30,14 +31,28 @@ UI.prototype = {
   render: function(fixtures){
     var container = document.getElementById("fixtures-container");
     container.innerHTML = "";
-    console.log(fixtures);
     for (var fixture of fixtures) {
+      url = "http://api.openweathermap.org/data/2.5/weather?lat=" + fixture.stadium.latLng.lat + "&lon=" + fixture.stadium.latLng.lng + "&appid=d1da5efdf6bd32c103ff303597e79de2";
+      this.requestHelper.makeRequest(url, this.populateWeather )
       var li = document.createElement('li');
       this.appendText(li, fixture.homeTeamName, 'Home: ');
       this.appendText(li, fixture.awayTeamName, 'Away: ');
 
       container.appendChild(li);
     }
+  },
+  populateWeather: function(location){
+    console.log(location)
+    var ul = document.getElementById('weatherAPI');
+    var li = document.createElement('li')
+    li.innerText = "Weather type: " + location.weather[0].main
+
+    var li2 = document.createElement('li')
+    var tempCelsius = Math.round((location.main.temp - 273.15))
+    li2.innerText = "Temperature (celsius): " + tempCelsius
+
+    ul.appendChild(li)
+    ul.appendChild(li2)
   }
 }
 
