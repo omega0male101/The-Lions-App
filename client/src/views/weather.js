846b15a9288e00b063189512b811b596cd80154b
Benjamin <benjamin.kinnard1@gmail.com>
2017-05-26 12:49:12 +0100

    added temp in celsius

--- client/src/views/weather.js
+++ client/src/views/weather.js
@@ -1,9 +1,5 @@
 var weather = function() {
 
-  var url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=d1da5efdf6bd32c103ff303597e79de2"
-
-  var url2 = "http://api.openweathermap.org/data/2.5/weather?lat=-35.734070&lon=174.329517&appid=d1da5efdf6bd32c103ff303597e79de2"
-
   var url3 = urlConstructor(-35.734070, 174.329517);
 
   makeRequest(url3, requestComplete)
@@ -35,7 +31,13 @@ var populateWeather = function(location){
   var ul = document.getElementById('weatherAPI');
   var li = document.createElement('li')
   li.innerText = "Weather type: " + location.weather[0].main
+
+  var li2 = document.createElement('li')
+  var tempCelsius = Math.round((location.main.temp - 273.15))
+  li2.innerText = "Temperature (celsius): " + tempCelsius
+
   ul.appendChild(li)
+  ul.appendChild(li2)
 }
 
 window.addEventListener('load', weather);
